4206aa9b1fa3ac828f56baeea325bf71a8cd4406
ebinjoe <89036851+ebinjoe@users.noreply.github.com>
2022-03-22 15:30:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./Header";
+
+function App() {
+  return (
+ <div>
+<div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
+<div className="max-w-screen-2xl mx-auto">
+<Header/>
+</div>
+</div>
+
+
+ </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,213 @@
+import react from "react";
+import { useMoralis } from "react-moralis";
+
+import React, { useState } from "react";
+function Header() {
+  const [navbar, setnavbar] = useState(false);
+  const [navprofile, setnavprofile] = useState(false);
+
+  const toggleNav = () => {
+    setnavbar(!navbar);
+  };
+
+  const toggleprofile = () => {
+    setnavprofile(!navprofile);
+  };
+
+  return (
+    <div>
+      <nav class="bg-gray-800">
+        <div class="mx-auto px-1 sm:px-6 lg:px-4 flex justify-between mysecstyle">
+          <div class=" inset-y-0 left-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div class="flex-shrink-0 flex items-center hidden">
+              <img
+                class="block lg:hidden h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                alt="Workflow"
+              />
+              <img
+                class="hidden lg:block h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                alt="Workflow"
+              />
+            </div>
+
+            <div class="ml-3  mystyle">
+              <div>
+                <button
+                  type="button"
+                  class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  id="user-menu-button"
+                  aria-expanded="false"
+                  aria-haspopup="true"
+                  onClick={toggleprofile}
+                >
+                  <span class="sr-only">Open user menu</span>
+                  <img
+                    class="h-8 w-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </button>
+
+                {navprofile && (
+                  <div
+                    class="origin-top-right absolute left-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu-button"
+                    tabindex="-1"
+                  >
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabindex="-1"
+                      id="user-menu-item-0"
+                    >
+                      Your Profile
+                    </a>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabindex="-1"
+                      id="user-menu-item-1"
+                    >
+                      Settings
+                    </a>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabindex="-1"
+                      id="user-menu-item-2"
+                    >
+                      Sign out
+                    </a>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+          <div class=" flex items-center justify-between h-16">
+            <div class=" inset-y-0 left-0 flex items-center sm:hidden">
+              <button
+                type="button"
+                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+                onClick={toggleNav}
+              >
+                <span class="sr-only">Open main menu</span>
+
+                <svg
+                  class="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+
+                <svg
+                  class="hidden h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div class="hidden sm:block sm:ml-6">
+                <div class="flex space-x-4">
+                  <a
+                    href="#"
+                    class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                    aria-current="page"
+                  >
+                    Dashboard
+                  </a>
+
+                  <a
+                    href="#"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Team
+                  </a>
+
+                  <a
+                    href="#"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Projects
+                  </a>
+
+                  <a
+                    href="#"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Calendar
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="sm:hidden" id="mobile-menu">
+          {navbar && (
+            <div class="px-2 pt-2 pb-3 space-y-1">
+              <a
+                href="#"
+                class="  text-white block px-3 py-2 rounded-md text-base font-medium text-right"
+                aria-current="page"
+              >
+                Dashboard
+              </a>
+
+              <a
+                href="#"
+                class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium text-right"
+              >
+                Team
+              </a>
+
+              <a
+                href="#"
+                class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium text-right"
+              >
+                Projects
+              </a>
+
+              <a
+                href="#"
+                class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium text-right"
+              >
+                Calendar
+              </a>
+            </div>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
